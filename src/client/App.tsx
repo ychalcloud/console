de9431e3720635cc23c7de5b720babe1a718c391
Lee Seungmin <lee@seungmin.io>
2020-04-28 09:43:25 +0900

    fix: Routing for register page

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Route, Switch } from "react-router";
 import { BrowserRouter } from "react-router-dom";
-import { Root, Login } from "../pages";
+import { Root, Login, Register } from "../pages";
 
 const App: React.FC = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Root} />
       <Route exact path="/auth/login" component={Login} />
+      <Route exact path="/auth/register" component={Register} />
     </Switch>
   </BrowserRouter>
 );
