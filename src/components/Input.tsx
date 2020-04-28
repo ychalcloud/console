1d3aeb2f9669396fd7ff1facafe2baeb87198121
Lee Seungmin <lee@seungmin.io>
2020-04-28 09:42:55 +0900

    feat: select component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -36,3 +36,23 @@ export const Input = styled.input`
 export const BigInput = styled(Input)`
   font-size: 24px;
 `;
+
+export const Select = styled.select`
+  background: white;
+  font-size: 16px;
+  border: 0;
+  outline: 0;
+  border: 1px solid rgb(180, 180, 180);
+  padding: 13px 15px;
+   appearance: none;
+  border-radius: 5px;
+  transition: border-color 0.2s linear;
+  & :focus {
+    border-width: 2px;
+    border-color: #b45bff;
+  }
+`;
+
+export const BigSelect = styled(Select)`
+  font-size: 24px;
+`;
