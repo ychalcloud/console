c946cf97ed7d693e81070e151b54c153d83a68e9
Lee Seungmin <lee@seungmin.io>
2020-05-02 11:07:55 +0900

    feat: instance page

---
+++ src/pages/Instance.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import ServiceWrapper from "../components/ServiceWrapper";
+
+import {
+  PageHeader,
+  IconWrap,
+  HeaderText,
+  DescriptionText
+} from "../components/PageHeader";
+
+import { AiOutlineCloudServer } from "react-icons/ai";
+
+const Instance: React.FC = () => {
+  return (
+    <ServiceWrapper>
+      <PageHeader>
+        <IconWrap>
+          <AiOutlineCloudServer color="white" size={70} />
+        </IconWrap>
+        <HeaderText>Instance</HeaderText>
+        <DescriptionText>
+          에이클라우드플랫폼이 제공하는 인스턴스 자동화 시스템입니다. <br />
+          에이클라우드플랫폼만의 차별화된 디도스 보안존을 누려보세요.
+        </DescriptionText>
+      </PageHeader>
+    </ServiceWrapper>
+  );
+};
+
+export default Instance;
