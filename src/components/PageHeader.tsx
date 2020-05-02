f8eee8f528cb222c15da37c445c60c406368eb3c
Lee Seungmin <lee@seungmin.io>
2020-05-02 11:05:33 +0900

    feat: PageHeader component

---
+++ src/components/PageHeader.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+export const IconWrap = styled.div`
+  margin-bottom: 15px;
+`;
+
+export const PageHeader = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 220.72px;
+  padding: 20px;
+  color: white;
+  background: linear-gradient(45deg, #ea6969, #b45bff);
+`;
+
+export const HeaderText = styled.h1`
+  font-size: 24px;
+  font-weight: 700;
+  margin-bottom: 15px;
+`;
+
+export const DescriptionText = styled.span`
+  font-size: 16px;
+  line-height: 1.5;
+`;
