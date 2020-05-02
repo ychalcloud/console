a6f32b443110f0f0a9aeb791e2af1c7de9824558
Lee Seungmin <lee@seungmin.io>
2020-05-02 11:08:19 +0900

    feat:  auth/info page

---
+++ src/pages/MyInfo.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import ServiceWrapper from "../components/ServiceWrapper";
+
+import {
+  PageHeader,
+  IconWrap,
+  HeaderText,
+  DescriptionText
+} from "../components/PageHeader";
+
+import { AiOutlineUser } from "react-icons/ai";
+
+const MyInfo: React.FC = () => {
+  return (
+    <ServiceWrapper>
+      <PageHeader>
+        <IconWrap>
+          <AiOutlineUser color="white" size={70} />
+        </IconWrap>
+        <HeaderText>My Info</HeaderText>
+        <DescriptionText>
+          에이클라우드플랫폼에 안전하게 저장되어 있는 고객님의 정보입니다.
+          <br />
+          정보 확인 혹은 변경 중 생기는 문제는 카카오톡 플러스친구 @에이클라우드플랫폼 으로 연락주세요.
+        </DescriptionText>
+      </PageHeader>
+    </ServiceWrapper>
+  );
+};
+
+export default MyInfo;
