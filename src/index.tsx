1104349c6af4324e0b197249b1f85f98f0333fb7
Lee Seungmin <lee@seungmin.io>
2020-05-02 11:09:49 +0900

    fix: load ag-grid css

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,9 @@ import './styles/fonts/fonts.css';
 import './styles/reset.css';
 import './styles/index.css';
 
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-balham.css';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 serviceWorker.unregister();
